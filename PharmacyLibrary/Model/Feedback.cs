b22ca329fa73dd61ba983cb1e08ad2b7cc85fe88
Dajana Zlokapa <zlokapa.ra240.2018@uns.ac.rs>
2021-11-03 22:18:54 +0100

    build: startup application

---
+++ PharmacyLibrary/Model/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyLibrary.Model
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int IdPharmacy { get; set; }
+    }
+}
---
+++ PharmacyLibrary/Model/Medicine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrugstoreLibrary.Model
+{
+   public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Intensity { get; set; }
+        public bool IsRecipe { get; set; }
+
+    }
+}
