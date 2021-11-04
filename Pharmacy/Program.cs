fa6ff257f4c7a7bd5fe328513c848a8ba6ddb5f1
simin161 <simin.ra161.2018@uns.ac.rs>
2021-11-04 19:41:19 +0100

    fix: deleted using line

--- Pharmacy/Program.cs
+++ Pharmacy/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Pharmacy.Controllers;
-using PharmacyLibrary.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
