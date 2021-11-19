c3776269037adc000794028f245b73964058d83b
poparicsara <73502384+poparicsara@users.noreply.github.com>
2021-11-19 20:46:05 +0100

    test: integration tests added
    * test: pharmacy offer tests created
    
    * test: integrations tests added

--- PharmacyLibrary/Model/DatabaseContext.cs
+++ PharmacyLibrary/Model/DatabaseContext.cs
@@ -8,6 +8,10 @@ namespace PharmacyLibrary.Model
 {
     public class DatabaseContext:DbContext
     {
+        public DatabaseContext()
+        {
+        }
+
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
         public DbSet<Medicine> Medicines { get; set; }
         public DbSet<Pharmacy> Pharmacies { get; set; }
@@ -15,5 +19,13 @@ namespace PharmacyLibrary.Model
         public DbSet<Feedback> Feedbacks { get; set; }
         public DbSet<FeedbackResponse> FeedbackResponses { get; set; }
         public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;port=5432;database=drugstoredb;username=root;password=root");
+            base.OnConfiguring(optionsBuilder);
+        }
+
     }
 }
