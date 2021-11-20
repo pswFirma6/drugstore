51b5b68c3756ac4f78958b48bd59bab88a5c578b
milicaoui <73649177+milicaoui@users.noreply.github.com>
2021-11-20 17:46:40 +0100

    feat: communication with sftp server
    * feat: making medicine names available to get
    
    * test: added unit and integration test
    
    * fix: getting rid of multiple dbContexts
    
    * test: added test for checking connection

---
+++ PharmacyAppTests/UnitTests/ReportUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model.Enums;
+using PharmacyLibrary.Services;
+using PhramacyLibrary.Model;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PharmacyAppTests.UnitTests
+{
+    public class ReportUnitTests
+    {
+
+        private ReportsService service;
+        [Fact]
+        public void CheckIfMedicineExists()
+        {
+            var stubRepo = new Mock<IMedicineRepository>();
+            service = new ReportsService(stubRepo.Object);
+            Medicine med = new Medicine(1, "Brufen", "Manufactorer", MedicineType.ANALGESIC, "", false, "none", "none", 1, 1);
+            List<Medicine> medicines = new List<Medicine>();
+            medicines.Add(med);
+            stubRepo.Setup(m => m.GetAll()).Returns(medicines);
+
+            Medicine m = service.GetMedicine("nonexistant medicine");
+
+            m.ShouldBeNull();
+        }
+    }
+}
