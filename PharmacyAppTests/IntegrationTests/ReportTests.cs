acc1afa7ec48450c489572870c963af48e303f44
milicaoui <vumilicaamm@gmail.com>
2021-11-20 16:07:58 +0100

    test: added unit and integration test

---
+++ PharmacyAppTests/IntegrationTests/ReportTests.cs
@@ -0,0 +1,30 @@
+﻿using Renci.SshNet;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace PharmacyAppTests.IntegrationTests
+{
+    public class ReportTests
+    {
+        [Fact]
+        public void CheckifFileExists()
+        {
+            String fileName = @"data\public\MedicationConsumptionReport.txt";
+            bool exists = false;
+
+            using (SftpClient client = new SftpClient(new PasswordConnectionInfo("192.168.56.1", "tester", "password")))
+            {
+                client.Connect();
+                if (File.Exists(fileName))
+                    exists = true;
+                client.Disconnect();
+            }
+
+            exists.ShouldBe(true);
+        }
+    }
+}
