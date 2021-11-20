231f7ba570bbd994c3da7a9c53f866ddc8f56c2f
poparicsara <73502384+poparicsara@users.noreply.github.com>
2021-11-21 00:10:13 +0100

    feat: add offer
    * feat: add offer
    
    * feat: get offers

---
+++ Pharmacy/Controllers/OfferController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model;
+using PharmacyLibrary.Repository;
+using PharmacyLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Controllers
+{
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        private OfferService service;
+        private IOfferRepository repository;
+
+        public OfferController(DatabaseContext dcontext)
+        {
+            repository = new OfferRepository(dcontext);
+            service = new OfferService(repository);
+        }
+
+        [HttpPost]
+        [Route("addOffer")]
+        public IActionResult AddOffer(Offer offer)
+        {
+            Offer newOffer = new Offer { Title = offer.Title, Content = offer.Content, StartDate = offer.StartDate, EndDate = offer.EndDate };
+            service.AddOffer(newOffer);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("getOffers")]
+        public List<Offer> GetOffers()
+        {
+            return service.GetOffers();
+        }
+    }
+}
