a266e1a98e7b9f2775bde1c1b59dfcb421d0a13c
NikolinaPavkovic <73259509+NikolinaPavkovic@users.noreply.github.com>
2021-12-20 14:05:52 +0100

    feat: rabbit mq for tender (#68)
    * feat: receiving tender from hospitals
    
    * feat: adding api key in appsettings file
    
    * feat: optimize rabbitMQ and add necessary files
    
    * fix: delete migrations
    
    * feat: send offer to hospital
    
    * fix: fix code smell

--- Pharmacy/Controllers/TenderController.cs
+++ Pharmacy/Controllers/TenderController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PharmacyLibrary.DTO;
+using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model;
+using PharmacyLibrary.Repository;
+using PharmacyLibrary.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +15,22 @@ namespace Pharmacy.Controllers
     [ApiController]
     public class TenderController
     {
-        
+        private readonly TenderOfferService tenderOfferService;
+        private readonly IConfiguration _config;
+
+        public TenderController(DatabaseContext databaseContext, IConfiguration config)
+        {
+            ITenderOfferRepository tenderOfferRepository = new TenderOfferRepository(databaseContext);
+            tenderOfferService = new TenderOfferService(tenderOfferRepository);
+            _config = config;
+        }
+
+        [HttpPost]
+        [Route("postTenderOffer")]
+        public void PostTenderOffer(TenderOfferDto dto)
+        {
+            var apiKey = _config.GetValue<string>("ApiKey");
+            tenderOfferService.AddTenderOffer(dto, apiKey);
+        }
     }
 }
