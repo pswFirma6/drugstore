c88537498b7384a9d1da89afbb5f0a8300c7db4d
milicaoui <vumilicaamm@gmail.com>
2022-01-06 01:28:53 +0100

    refactor: added date range to offer

--- PharmacyUnitTests/OfferTests.cs
+++ PharmacyUnitTests/OfferTests.cs
@@ -2,6 +2,7 @@
 using PharmacyLibrary.IRepository;
 using PharmacyLibrary.Model;
 using PharmacyLibrary.Services;
+using PharmacyLibrary.Shared;
 using Shouldly;
 using System;
 using System.Collections.Generic;
@@ -19,9 +20,9 @@ namespace PharmacyAppTests.UnitTests
         {
             var stubRepository = new Mock<IOfferRepository>();
             service = new OfferService(stubRepository.Object);
-            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", StartDate = new DateTime(2021, 11, 11), EndDate = new DateTime(2021, 11, 30) };
+            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", OfferDateRange = new DateRange(new DateTime(2021, 11, 11), new DateTime(2021, 11, 30)) };
 
-            bool checkDates = service.AreDatesAcceptable(offer.StartDate, offer.EndDate);
+            bool checkDates = service.AreDatesAcceptable(offer.OfferDateRange.StartDate, offer.OfferDateRange.EndDate);
 
             checkDates.ShouldBe(true);
         }
@@ -31,9 +32,9 @@ namespace PharmacyAppTests.UnitTests
         {
             var stubRepository = new Mock<IOfferRepository>();
             service = new OfferService(stubRepository.Object);
-            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", StartDate = new DateTime(2021, 11, 11), EndDate = new DateTime(2021, 09, 30) };
+            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", OfferDateRange = new DateRange(new DateTime(2021, 11, 11),  new DateTime(2021, 09, 30) )};
 
-            bool checkDates = service.AreDatesAcceptable(offer.StartDate, offer.EndDate);
+            bool checkDates = service.AreDatesAcceptable(offer.OfferDateRange.StartDate, offer.OfferDateRange.EndDate);
 
             checkDates.ShouldBe(false);
         }
@@ -44,7 +45,7 @@ namespace PharmacyAppTests.UnitTests
             var stubRepository = new Mock<IOfferRepository>();
             service = new OfferService(stubRepository.Object);
             List<Offer> offers = new List<Offer>();
-            Offer offer = new Offer { Id = 2, Title = "Offer1", Content = "Offer1", StartDate = new DateTime(2021, 11, 11), EndDate = new DateTime(2021, 11, 30) };
+            Offer offer = new Offer { Id = 2, Title = "Offer1", Content = "Offer1", OfferDateRange=new DateRange(new DateTime(2021, 11, 11), new DateTime(2021, 11, 30) )};
 
             stubRepository.Setup(m => m.Add(offer)).Callback((Offer o) => offers.Add(o));
 
@@ -60,7 +61,7 @@ namespace PharmacyAppTests.UnitTests
             service = new OfferService(stubRepository.Object);
 
             List<Offer> offers = new List<Offer>();
-            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", StartDate = new DateTime(2021, 11, 11), EndDate = new DateTime(2021, 11, 30) };
+            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", OfferDateRange = new DateRange(new DateTime(2021, 11, 11), new DateTime(2021, 11, 30) )};
             offers.Add(offer);
 
             stubRepository.Setup(m => m.GetAll()).Returns(offers);
