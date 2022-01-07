0ca911a6b6e501fa741d5d77652512792781bf33
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2022-01-07 15:51:34 +0100

    feat: Ads for medicines backend implemented

---
+++ PharmacyLibrary/Model/MedicineAd.cs
@@ -0,0 +1,29 @@
+﻿using PharmacyLibrary.Shared;
+using PhramacyLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyLibrary.Model
+{
+
+    public class MedicineAd
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateRange dateRange { get; }
+        public Medicine Medicine { get; set; }
+
+        public MedicineAd() { }
+
+        public MedicineAd(int id, string title, string description, DateRange dateRange, Medicine medicine)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            this.dateRange = dateRange;
+            Medicine = medicine;
+        }
+    }
+}
---
+++ PharmacyLibrary/Repository/MedicineAdsRepository.cs
@@ -0,0 +1,16 @@
+﻿using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyLibrary.Repository
+{
+    public class MedicineAdsRepository: Repo<MedicineAd>, IMedicineAdsRepository
+    {
+        public MedicineAdsRepository(DatabaseContext context) : base(context)
+        {
+
+        }
+    }
+}
