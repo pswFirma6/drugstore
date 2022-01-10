15b0d3f164006b2ddef9f4d63a53ee35ac4af6b2
jandric162 <73339968+jandric162@users.noreply.github.com>
2022-01-10 19:06:42 +0100

    feat: event store (#78)
    * feat: backend for event tracking and storing in database
    
    * fix: fixed errors with repository
    
    * ops: modified gitignore
    
    * feat: event clocking
    
    * refactor: removing code smells
    
    * ops: edited gitignore
    
    * refactor: removed conflicts

---
+++ PharmacyLibrary/Model/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyLibrary.Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ClickTime { get; set; }
+        public string ApplicationName { get; set; }
+
+    }
+}
--- PharmacyLibrary/Services/HospitalService.cs
+++ PharmacyLibrary/Services/HospitalService.cs
@@ -1,3 +1,74 @@
+<<<<<<< HEAD
+using PhramacyLibrary.Model;
+using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model;
+using PharmacyLibrary.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace PharmacyLibrary.Services
+{
+    public class HospitalService
+    {
+
+        private readonly IHospitalRepository hospitalRepository;
+        public HospitalService(IHospitalRepository iRepository)
+        {
+            hospitalRepository = iRepository;
+        }
+
+        public List<Hospital> GetAll()
+        {
+            return hospitalRepository.GetAll();
+        }
+
+        public void AddHospital(HospitalInfo hospital,PersonalInfo info)
+        {
+            var client = new RestClient(hospital.Url + "registerPharmacy");
+            var request = new RestRequest();
+
+
+            request.AddJsonBody(info);
+            var res = client.Post(request);
+            
+            String apiKey = res.Content;
+            if(apiKey.Length == 0)
+                apiKey = "jaksjdhagshjikps";
+            apiKey = apiKey.Substring(1, apiKey.Length - 2);
+
+
+            ConnectionInfo conInfo = new ConnectionInfo(hospital.Url, apiKey);
+            Address address = new Address(hospital.HospitalAddress,hospital.HospitalCity);
+
+            Hospital newHospital = new Hospital(hospital.HospitalName, address, conInfo);
+
+            AddHospital(newHospital);
+        }
+
+        public void AddHospital(Hospital hospital)
+        {
+            hospitalRepository.Add(hospital);
+            hospitalRepository.Save();
+        }
+
+        public bool CheckHospitalName(string hospital)
+        {
+            bool duplicate = false;
+            foreach (Hospital currHospital in hospitalRepository.GetAll())
+            {
+                if (currHospital.HospitalName.Equals(hospital))
+                {
+                    duplicate = true;
+                    break;
+                }
+            }
+            return duplicate;
+        }
+    }
+}
+=======
 using PhramacyLibrary.Model;
 using PharmacyLibrary.IRepository;
 using PharmacyLibrary.Model;
@@ -66,3 +137,4 @@ namespace PharmacyLibrary.Services
         }
     }
 }
+>>>>>>> develop
