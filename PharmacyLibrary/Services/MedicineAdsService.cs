14cf210695882f4d69b8c070a77acf169520968b
Magdalena Reljin <73749234+magdalenaRA822018@users.noreply.github.com>
2022-01-10 19:10:03 +0100

    feat: medicine ads (#80)
    * feat: Ads for medicines backend implemented
    
    * fix: resolved confilcts

---
+++ PharmacyLibrary/Services/MedicineAdsService.cs
@@ -0,0 +1,33 @@
+﻿using PharmacyLibrary.IRepository;
+using PharmacyLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyLibrary.Services
+{
+    public class MedicineAdsService
+    {
+        private readonly IMedicineAdsRepository medicineAdsRepository;
+        public MedicineAdsService(IMedicineAdsRepository iRepository)
+        {
+            medicineAdsRepository = iRepository;
+        }
+        public List<MedicineAd> GetAll()
+        {
+            return medicineAdsRepository.GetAll();
+        }
+        public void AddMedicineAd(MedicineAd medicineAd)
+        {
+            medicineAd.Id = medicineAdsRepository.GetAll().Count + 1;
+            medicineAdsRepository.Add(medicineAd);
+            medicineAdsRepository.Save();
+        }
+
+        public void DeleteMedicineAd(MedicineAd medicineAd)
+        {
+            medicineAdsRepository.Delete(medicineAd.Id);
+            medicineAdsRepository.Save();
+        }
+    }
+}
